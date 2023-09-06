b3e5c9c4568494343e41688be4f6e394d2d380da
Huseyin Cem Bas <huseyincembas@outlook.com>
2023-09-06 21:49:44 +0300

    first commit

---
+++ client/src/components/ProtectedPage.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react'
+import { message } from 'antd'
+import { GetCurrentUser } from '../apicalls/users'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { SetLoader } from '../redux/loadersSlice'
+import { SetUser } from '../redux/usersSlice'
+
+function ProtectedPage({ children }) {
+    const {user} = useSelector((state) => state.users);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const validateToken = async () => {
+        try {
+            dispatch(SetLoader(true))
+            const response = await GetCurrentUser()
+            dispatch(SetLoader(false))
+            if (response.success) {
+                dispatch(SetUser(response.data))
+            } else {
+                navigate("/login")
+                message.error(response.message)
+            }
+        } catch (error) {
+            dispatch(SetLoader(false))
+            navigate("/login")
+            message.error(error.message);
+        }
+    }
+
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            validateToken()
+        } else {
+            navigate("/login")
+        }
+    }, []);
+
+    return (
+        user && (
+            <div>
+
+                {/* header */}
+                <div className='flex justify-between items-center bg-primary p-5'>
+                    <h1 className='text-2xl text-white cursor-pointer'
+                        onClick={() => navigate("/")}
+                    >
+                        REAS STORE
+                    </h1>
+                    <div className='bg-white py-2 px-5 rounded flex gap-1 items-center'>
+                        <i className="ri-user-line"></i>
+                        <span className='underline cursor-pointer uppercase'
+                            onClick={() => {
+                               if (user.role === 'user'){
+                                navigate("/profile")
+                               }else{
+                                navigate('/admin')
+                               }
+                            }}
+                        >{user.name}</span>
+                        <i className="ri-login-box-line ml-10"
+                        onClick={() =>{
+                            localStorage.removeItem("token")
+                            navigate("/login")
+                        }}
+                        ></i>
+                    </div>
+                </div>
+                <div className='p-5'>{children}</div>
+            </div>
+        )
+    )
+}
+
+
+export default ProtectedPage
---
+++ server/routes/productsRoute.js
@@ -0,0 +1,147 @@
+const router = require('express').Router();
+const Product = require('../models/productModel')
+const authMiddleware = require('../middlewares/authMiddleware')
+const cloudinary = require('../config/cloudinaryConfig')
+const multer = require('multer')
+
+// add a new product
+router.post("/add-product", authMiddleware, async (req, res) => {
+    try {
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        res.send({
+            success: true,
+            message: "Product added successfully",
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+//get all products
+
+router.post("/get-products", async (req, res) => {
+    try {
+        const { seller, categories = [], age = [] } = req.body
+        let filters = {}
+        if (seller) {
+            filters.seller = seller
+        }
+        const products = await Product.find(
+            filters
+        ).populate('seller').sort({ createdAt: -1 });
+        res.send({
+            success: true,
+            data: products,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+//get a product by id
+router.get("/get-product-by-id/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id).populate("seller");
+        res.send({
+            success: true,
+            data: product,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+// edit a product
+router.put("/edit-product/:id", authMiddleware, async (req, res) => {
+    try {
+        await Product.findByIdAndUpdate(req.params.id, req.body);
+        res.send({
+            success: true,
+            message: "Product updated successfully",
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        })
+    }
+})
+
+//delete a product
+router.delete("/delete-product/:id", authMiddleware, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.send({
+            success: true,
+            message: "Product deleted successfully"
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        })
+    }
+})
+
+
+// get image from pc
+
+const storage = multer.diskStorage({
+    filename: function (req, file, callback) {
+        callback(null, Date.now() + file.originalname)
+    },
+});
+
+router.post('/upload-image-to-product', authMiddleware, multer({ storage: storage }).single('file',), async (req, res) => {
+    try {
+        //upload image to cloudinary
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            folder: "reasstore"
+        })
+
+        const productId = req.body.productId;
+        await Product.findByIdAndUpdate(productId, {
+            $push: { images: result.secure_url },
+        })
+        res.send({
+            success: true,
+            message: "Image uploaded successfully",
+            data: result.secure_url,
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        })
+    }
+});
+
+//update product status
+
+router.put("/update-product-status/:id", authMiddleware, async (req, res) => {
+    try {
+        const { status } = req.body;
+        await Product.findByIdAndUpdate(req.params.id, { status });
+        res.send({
+            success: true,
+            message: "Product status updated successfully",
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        })
+    }
+})
+
+module.exports = router;
